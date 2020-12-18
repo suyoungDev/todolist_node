e3137f5eb204a14ce4afdc9ab14b72e248662877
mocha <syda07@gmail.com>
2020-12-18 21:52:03 +0900

    추가: procfile, update ports

--- app.js
+++ app.js
@@ -126,6 +126,9 @@ app.get('/about', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('server started on port 3000');
-});
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, () => console.log('server started'))
